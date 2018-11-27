eb5a3fd56520b023d1c190f1aa601625b7256300
K Mohandas, Sandeep <sandeep.k.mohandas@accenture.com>
2018-11-27 11:12:21 +0530

    Add project files.

---
+++ algorithms/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace algorithms
+{
+
+    public  class node
+    {
+        public  Object value { get
+                
+                ; set
+                
+                ; }
+      public   node next { get; set; }
+    }
+
+    public  class LinkedList<T> {
+
+       
+
+        public node Head { get; set;
+        }
+
+        public node Tail {
+            get;
+
+            set;
+
+        }
+
+        public node LinkedListnode {
+            get;
+            set;
+        }
+       
+ 
+        public void AddtoHead(node Node)
+        {
+            
+            node CurrentNode = Head;
+            Head = Node;
+            Head.next = CurrentNode;
+            
+        }
+        public void AddtoTail(node Node)
+        {
+            Tail.next = Node;
+            Tail = Node;
+            Tail.next = null;
+            
+        }
+
+        public IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+
+            node current = Head;
+            while (current.value != null)
+            {
+                yield return current.value;
+                current = current.next;
+            }
+
+        }
+      
+       
+    }
+
+    public class LinkedListOps
+    {
+
+        public void Addtolinkedlist(node node) {
+
+            Console.WriteLine("Add to Head or Tail type H or T");
+            var x = Console.ReadLine();
+            if (x == "H")
+            {
+                //add to Head
+
+              
+            }
+        }
+
+   
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
