84ed5902bb6800205c2232c8fb51d7a13aefef83
K Mohandas, Sandeep <sandeep.k.mohandas@accenture.com>
2018-11-29 11:39:39 +0530

    upddated linkedlist node class and linkedlist class

--- algorithms/Program.cs
+++ algorithms/Program.cs
@@ -14,55 +14,162 @@ namespace algorithms
       public   node next { get; set; }
     }
 
-    public  class LinkedList<T> {
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+        public LinkedListNode<T> Next { get; set; }
+
+       
+    }
+
+
+    public class LinkedList<T>:ICollection<T>{
 
        
 
-        public node Head { get; set;
-        }
-
-        public node Tail {
+        public LinkedListNode<T> Head {
             get;
 
-            set;
+          private  set;
+        }
+
+        public LinkedListNode<T> Tail {
+            get;
+
+           private set;
 
         }
 
-        public class LinkedListnode<T>
+        public int Count { get; private set; }
+
+        public bool IsReadOnly { get
+            { return false; } }
+
+        int ICollection<T>.Count { get; }
+
+        bool ICollection<T>.IsReadOnly { get { return false; } }
+
+        public void Add(T item)
         {
-            public T  value { get; set; }
-            public LinkedListNode<T> next { get; set; }
+            AddtoHead(new LinkedListNode<T>(item));
         }
- 
-        public void AddtoHead(node Node)
+
+        public void AddtoHead(LinkedListNode<T> linkedListnode)
+        {
+
+            LinkedListNode<T> CurrentNode = Head;
+            Head = linkedListnode;
+            Head.Next = CurrentNode;
+            Count++;
+        }
+        public void AddtoTail(LinkedListNode<T> linkedListNode)
+        {
+            Tail.Next = linkedListNode;
+            Tail = linkedListNode;
+            Tail.Next = null;
+            Count++;
+         }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+         IEnumerator<T> IEnumerable<T>.GetEnumerator()
         {
             
-            node CurrentNode = Head;
-            Head = Node;
-            Head.next = CurrentNode;
-            
-        }
-        public void AddtoTail(node Node)
-        {
-            Tail.next = Node;
-            Tail = Node;
-            Tail.next = null;
-            
-        }
-
-        public IEnumerator<T> IEnumerable<T>.GetEnumerator()
-        {
-
             LinkedListNode<T> current = Head;
-            while (current.value != null)
+            if (Count == 0)
             {
-                yield return current.value;
-                current = current.next;
+                yield return current.Value;
             }
-
+            else
+            { 
+            while (current.Next != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+            }
         }
-      
-       
+
+        
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<T>.Add(T item)
+        {
+            AddtoHead(new LinkedListNode<T>(item));
+        }
+
+        void ICollection<T>.Clear()
+        {
+            Head = Tail = null;
+            Count = 0;
+        }
+
+        bool ICollection<T>.Contains(T item)
+        {
+            if (Count == 0)
+                return false;
+            else
+            {
+                LinkedListNode<T> current = Head;
+                while (current.Next != null)
+                {
+                    if (current.Value.Equals(item))
+                        return true;
+                    else
+                        current = current.Next;
+
+                }
+                return false;
+            }
+        }
+
+        void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICollection<T>.Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            LinkedListNode<T> current = Head;
+            if (Count == 0)
+                yield return Head.Value;
+            else
+            {
+                while (current.Next!=null)
+                {
+                    yield return current.Value;
+                    current = current.Next;
+
+                }
+            }
+        }
     }
 
     public class LinkedListOps
@@ -80,15 +187,22 @@ namespace algorithms
             }
         }
 
-   
+    
     }
 
-
+     
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+
+
+            LinkedList<int> vs = new LinkedList<int>();
+           
+
         }
     }
 }
